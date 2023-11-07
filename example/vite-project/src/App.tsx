0d2dcaeb763241729bae9f7814009b6d457297de
crackwig <montajebi.tech@gmail.com>
2023-11-07 15:33:05 +0330

    ADD: vite example

---
+++ example/vite-project/src/App.tsx
@@ -0,0 +1,20 @@
+import { SamanIcon, SamanColorIcon } from "iraninan-banks-react-icons";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <SamanIcon className="logo" />
+        <SamanColorIcon className="logo" />
+      </div>
+      <h1>React Iran Banks Logos</h1>
+
+      {/* <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  );
+}
+
+export default App;
